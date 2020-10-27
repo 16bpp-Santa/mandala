ddb5d75ba51d63d6eba4ac135d33a1ef29730c70
Santa <71898556+16bpp-Santa@users.noreply.github.com>
2020-10-27 00:00:08 -0300

    mandala.py
    This is the code. You need to install turtle module for python

---
+++ mandala.py
@@ -0,0 +1,18 @@
+import turtle
+t = turtle.Pen()
+n=0
+k=1
+g=10
+t.speed(9999)
+while 1==1:
+    for n in range(0,2):
+        t.forward(30)
+        t.forward(30)
+        t.forward(30)
+        t.left(3)
+        n=n+1
+    
+    t.right(100)
+    t.right(10)
+    t.right(30)
+    n==0
